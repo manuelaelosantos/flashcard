ca6414cbe4c6d48bae0449fd7c3a7072694f16a4
manuelaelosantos <163124866+manuelaelosantos@users.noreply.github.com>
2024-10-07 11:37:02 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Matemática',
+    'Quando é 2 + 2?'
+    "é igual a 4"
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Paraná?'
+    "Curitiba"
+)
+criaCartao(
+    'História',
+    'Qual era o país capitalista na guerra fria?'
+    "o EUA"
+)
+criaCartao(
+    'Biologia',
+    'Quantos ossos tem o corpo humano?'
+    "206 ossos"
+)
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem são os 3 deuses grandes na mitologia grega?'
+    "Zeus, Poseidon e Hades"
+)
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem escreveu a saga Harry Potter?"
+    'JK rolling'
+)
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem é o maior casal de todos os tempos da atual literatura'
+    "Percabeth"
+)
